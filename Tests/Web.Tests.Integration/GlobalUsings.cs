3d3dc6fe02f312cca1a9a60665d8eb50558c57c4
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-09-04 12:39:40 -0700

    feat(tests): add CreateCategoryTests for category creation functionality

---
+++ Tests/Web.Tests.Integration/GlobalUsings.cs
@@ -0,0 +1,22 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : BlazorBlogApplication
+// Project Name :  Web.Tests.Integration
+// =======================================================
+
+global using System.Diagnostics.CodeAnalysis;
+
+global using FluentAssertions;
+
+global using Microsoft.Extensions.DependencyInjection;
+
+global using MongoDB.Bson;
+global using MongoDB.Driver;
+
+global using Shared.Fakes;
+
+global using Web.Data;
+global using Web.Fixtures;
