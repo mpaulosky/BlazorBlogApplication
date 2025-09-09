d9427b0ea2419ed595189e79aec1a74e302bca79
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-09-09 08:36:20 -0700

    refactor(FakeCategory): update Id generation in Faker setup
    - Change Id generation to use a lambda expression.
    - Improve clarity and consistency in Faker configuration.

--- SRC/Shared/Fakes/FakeCategory.cs
+++ SRC/Shared/Fakes/FakeCategory.cs
@@ -51,7 +51,7 @@ public class FakeCategory
 	{
 
 		var fake = new Faker<Category>()
-				.RuleFor(x => x.Id, f => ObjectId.GenerateNewId())
+				.RuleFor(x => x.Id, _ => ObjectId.GenerateNewId())
 				.RuleFor(x => x.CategoryName, _ => GetRandomCategoryName())
 				.RuleFor(x => x.IsArchived, f => f.Random.Bool())
 				.RuleFor(x => x.CreatedOn, _ => GetStaticDate())
